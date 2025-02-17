ccd03b71577b14db3f80c7c2c1e7a0b7d0268975
Akshada Jadhav <92100928+akshadajadhav01@users.noreply.github.com>
2025-02-17 21:34:17 +0530

    test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
-import connectDB from "./database/db.js";  
-import cookieParser from "cookie-parser"; 
+import connectDB from "./database/db.js";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.router.js";
@@ -14,21 +14,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:3001", // Next.js frontend URL
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Next.js frontend URL
     credentials: true, // Allow cookies to be sent
-  }));
+  })
+);
 app.use(errorMiddleware);
 
-
-
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.listen(process.env.PORT, async () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
-    await connectDB();
+  console.log(`Server listening on port ${process.env.PORT}`);
+  await connectDB();
 });
 
 export default app;
